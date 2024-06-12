e0d732236c2a04112751c744a848c0e831753ad6
ms <ms@Miguels-MacBook-Pro.local>
2024-06-12 22:17:20 +0200

    implement navbar

--- context/ThemeProvider.tsx
+++ context/ThemeProvider.tsx
@@ -1,6 +1,6 @@
 'use client';
 
-import React, { createContext, useState, useContext, useEffect } from 'react';
+import React, { createContext, useContext, useState, useEffect } from 'react';
 
 interface ThemeContextType {
 	mode: string;
@@ -13,12 +13,16 @@ export function ThemeProvider({ children }: { children: React.ReactNode }) {
 	const [mode, setMode] = useState('');
 
 	const handleThemeChange = () => {
-		if (mode === 'dark') {
-			setMode('light');
-			document.documentElement.classList.add('light');
-		} else {
+		if (
+			localStorage.theme === 'dark' ||
+			(!('theme' in localStorage) &&
+				window.matchMedia('(prefers-color-scheme: dark)').matches)
+		) {
 			setMode('dark');
 			document.documentElement.classList.add('dark');
+		} else {
+			setMode('light');
+			document.documentElement.classList.remove('dark');
 		}
 	};
 
@@ -39,5 +43,6 @@ export function useTheme() {
 	if (context === undefined) {
 		throw new Error('useTheme must be used within a ThemeProvider');
 	}
+
 	return context;
 }
