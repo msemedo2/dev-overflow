68c7169429ebae096477b82bf150563059fdd495
ms <ms@ip-192-168-178-58.eu-west-1.compute.internal>
2024-07-15 20:40:07 +0200

    implement global filter and homepage filter

--- app/(root)/(home)/page.tsx
+++ app/(root)/(home)/page.tsx
@@ -1,5 +1,8 @@
+import HomeFilters from '@/components/Home/HomeFilters';
+import Filter from '@/components/shared/Filter';
 import LocalSearchBar from '@/components/shared/search/LocalSearchBar';
 import { Button } from '@/components/ui/button';
+import { HomePageFilters } from '@/constants/filtes';
 import Link from 'next/link';
 import React from 'react';
 
@@ -24,8 +27,14 @@ const Home = () => {
 					placeholder="Search for questions"
 					otherClasses="flex-1"
 				/>
-				Filters
+				<Filter
+					filters={HomePageFilters}
+					otherClasses="min-h-[56px] sm:min-w-[170px]"
+					containerClasses="hidden max-md:flex"
+				/>
 			</div>
+
+			<HomeFilters />
 		</>
 	);
 };
