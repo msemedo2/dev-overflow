80a3ad9c09cd963bded9d95344262045163a2198
ms <ms@ip-192-168-178-58.eu-west-1.compute.internal>
2024-08-20 19:12:42 +0200

    add api webhook to public routes

--- middleware.ts
+++ middleware.ts
@@ -2,8 +2,27 @@ import { clerkMiddleware, createRouteMatcher } from '@clerk/nextjs/server';
 
 const isProtectedRoute = createRouteMatcher(['/ask-question(.*)']);
 
+const isPublicRoute = createRouteMatcher([
+	'/',
+	'/api/webhook',
+	'question/:id',
+	'/tags',
+	'/tags/:id',
+	'/profile/:id',
+	'/community',
+	'/jobs',
+]);
+
+const isIgnoredRoute = createRouteMatcher(['/api/webhook', '/api/chatgpt']);
+
 export default clerkMiddleware((auth, req) => {
-	if (isProtectedRoute(req)) auth().protect();
+	if (isIgnoredRoute(req) || isPublicRoute(req)) {
+		return;
+	}
+
+	if (isProtectedRoute(req)) {
+		auth().protect();
+	}
 });
 
 export const config = {
